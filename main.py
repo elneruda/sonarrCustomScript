4b10be3899a8119a4cbd088fc8df2cf752478648
Paul NICOLAS <elneruda@gmail.com>
2019-05-01 00:52:18 +0200

    ♻️ - add missing val (releaseGroup), remove extra log

--- main.py
+++ main.py
@@ -153,11 +153,9 @@ link = tvMaze.getEpisodeUrl(season, episode)
 
 message = SlackMessage(webhook_url)
 message.package("*" +os.environ.get("sonarr_series_title", "") + " - " + season +"x"+ episode +" - " + os.environ.get("sonarr_episodefile_episodetitles", "") + "* ["+os.environ.get("sonarr_episodefile_quality", "")+"]")
-message.constructor("`"+ sonarr.indexer +"` " + sonarr.network)
+message.constructor("`"+ sonarr.indexer +"` _" + os.environ.get("sonarr_episodefile_releasegroup", "") + "_ " + sonarr.network)
 message.link(link)
 message.iconUrl = networkLogoUrl
 message.save()
-
-print(message)
 
 message.notify()
